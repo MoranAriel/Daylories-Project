8ad237cb272364155fad1b7910403d64dae4100b
Moran Ariel <141998962+MoranAriel@users.noreply.github.com>
2025-01-29 12:16:22 +0200

    init

---
+++ calories_system_backend/src/main/java/com/calories_system/CaloriesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.calories_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesSystemApplication.class, args);
+	}
+
+}
---
+++ calories_system_backend/src/test/java/com/calories_system/CaloriesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calories_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
